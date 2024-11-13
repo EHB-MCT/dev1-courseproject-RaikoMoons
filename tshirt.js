0b900861ba06349f4b49d09f85df4eb5e691e212
Raiko Moons <raiko.moons@student.ehb.be>
2024-11-14 00:02:07 +0100

    tshirt klaar dev oef 2 raiko

---
+++ tshirt.js
@@ -0,0 +1,68 @@
+"use strict";
+import context from "./scripts/context.js";
+import * as Utils from "./scripts/utils.js";
+
+let width = context.canvas.width;
+let lineWidth = 4;
+let circleswidth = 8;
+
+drawTshirtDesign();
+blackCircles();
+getRandomHue();
+
+function getRandomHue() {
+	return Utils.randomNumber(120, 360);
+}
+function drawTshirtDesign() {
+	context.lineWidth = lineWidth;
+	context.strokeStyle = Utils.hsl(getRandomHue(), 180, 40);
+	Utils.strokeCircle(width / 2, 350, 100, 100);
+
+	context.lineWidth = lineWidth;
+	context.strokeStyle = Utils.hsl(getRandomHue(), 180, 50);
+	Utils.strokeCircle(width / 2, 350, 120, 100);
+
+	context.lineWidth = lineWidth;
+	context.strokeStyle = Utils.hsl(getRandomHue(), 180, 40);
+	Utils.strokeCircle(width / 2, 350, 70, 100);
+
+	context.lineWidth = lineWidth;
+	context.strokeStyle = Utils.hsl(getRandomHue(), 180, 60);
+	Utils.strokeCircle(width / 2 + 100, 140, 40, 100);
+
+	context.lineWidth = lineWidth;
+	context.strokeStyle = Utils.hsl(getRandomHue(), 180, 20);
+	Utils.strokeCircle(width / 2 - 100, 180, 20, 100);
+
+	context.lineWidth = lineWidth;
+	context.strokeStyle = Utils.hsl(getRandomHue(), 180, 20);
+	Utils.strokeCircle(width / 2 + 100, 555, 40, 100);
+
+	context.lineWidth = lineWidth;
+	context.strokeStyle = Utils.hsl(getRandomHue(), 180, 40);
+	Utils.strokeCircle(width / 2 - 100, 500, 30, 100);
+
+	context.lineWidth = lineWidth;
+	context.strokeStyle = Utils.hsl(getRandomHue(), 180, 40);
+	for (let i = 1; i < 4; i++) {
+		Utils.strokeCircle(width / 2, 630, 30 + i * 10);
+	}
+}
+
+function blackCircles() {
+	context.lineWidth = circleswidth;
+	context.strokeStyle = "black";
+	Utils.strokeCircle(width / 2, 450, 75, 100);
+
+	context.lineWidth = circleswidth;
+	context.strokeStyle = "black";
+	Utils.strokeCircle(width / 2, 250, 40, 100);
+
+	context.lineWidth = circleswidth;
+	context.strokeStyle = "black";
+	Utils.strokeCircle(width / 1.75, 600, 80, 100);
+
+	context.lineWidth = circleswidth;
+	context.strokeStyle = "black";
+	Utils.strokeCircle(width / 1.75, 165, 30, 100);
+}
